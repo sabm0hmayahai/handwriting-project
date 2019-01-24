58443847f72d9a8450fdf66c90dbf7dcd8e8527e
Rajesh Hegde <rajeshhegde180@gmail.com>
2019-01-24 12:18:24 +0530

    final push

--- data processing.py
+++ data processing.py
@@ -22,6 +22,9 @@ for line in file:
     a.append(-p)
     b.append(q)
 
+'''plotting the unprocessed image for comparision with 
+the processed image later, this will not be uploaded to the cloud server'''
+
 gh=[0,0,0,-80]
 hg=[0,80,0,0]
 plt.plot(gh,hg)
@@ -29,7 +32,7 @@ plt.plot(a,b)
 
 plt.show()
 
-''' -p is stored in a because our origin is from the right
+''' (-p) is stored in 'a' because our origin is from the right
 unlike the common standard(from the left) supported
 by the matplotlib'''
 
@@ -48,10 +51,22 @@ nx = []
 ny = []
 k = 0
 
-'''alpha  tolerance is a key component in the vertical analysis'''
+'''alpha & tolerance is a key component in the vertical analysis
+    reduce alpha value to get more straight lines
+    increase it to get more smooth curves
+    increase tolerance to get more straight lines
+    reduce it to get smooth curves
+'''
+
+
+'''the average of 4 neighbours is taken for each value of a and b
+and the coordinate value is stored in lists x and y seperately'''
+
 alpha = 15
 
 tolerance = 2
+
+
 for i in range(0,length-(alpha+1),alpha):
     s = 0
     for j in range(i,i+alpha):
@@ -61,7 +76,6 @@ for i in range(0,length-(alpha+1),alpha):
     for j in range(i,i+alpha):
         s = s + abs(avg - x[j])
     avg_std_x = s/20
-    #print("average standard deviation of x is: ",avg_std_x)
     if(avg_std_x < tolerance): #if the deviation from mean indicates a vertical line
         nx.append(x[i])
         ny.append(y[i])
@@ -72,17 +86,10 @@ for i in range(0,length-(alpha+1),alpha):
             nx.append((x[j-4]+x[j-3]+x[j-2]+x[j-1]+x[j]+x[j+1]+x[j+2]+x[j+3]+x[j+4])/9)
             ny.append((y[j-4]+y[j-3]+y[j-2]+y[j-1]+y[j]+y[j+1]+y[j+2]+y[j+3]+y[j+4])/9)
         
-    #print("i= ",i)
 
 
 length = len(nx)
-'''the average of 4 neighbours is taken for each value of a and b
-and the coordinate value is stored in lists x and y seperately'''
 
-
-
-
-#for i,j in zip(a,b):
 
 '''will create a plot that with x and y values'''
 gh=[0,0,0,-80]
@@ -91,6 +98,7 @@ plt.plot(gh,hg)
 plt.plot(nx,ny)
 
 '''will show the plot, similar to the imshow function in opencv'''
+
 plt.show()
 
 gh=[0,0,0,-80]
@@ -102,11 +110,18 @@ plt.plot(nx,ny)
 '''if enabled, the command will save a pdf of the plotted figure'''
 '''NOTE: only enable ONE of plt.show() or plt.savefig() at a time
 i.e. either show or save figure'''
+
+'''for our reference'''
+
+plt.savefig("/Users/hegderajesh/Desktop/demo/demo.pdf")
+
+
 print("Successfully processed data session\n")
 print("\nPDF created\n")
 print("\nUploading to cloud . . . \n")
 
-plt.savefig("/Users/hegderajesh/Desktop/demo/demo.pdf")
+
+
 
 
 print("end of run\n")
